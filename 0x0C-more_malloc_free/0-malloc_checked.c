a984f6869991b3fa84691047aa31cf5b8665fd43
samsonjisso <samsonjisso300@gmail.com>
2024-05-09 16:13:41 +0300

    betty fix

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,13 +1,18 @@
 #include <stdlib.h>
 
+/**
+ * malloc_checked - memory allocater and checker function
+ * @b: interger to be allocated
+ * Return: pointer to the memory allocated
+*/
+
 void *malloc_checked(unsigned int b)
 {
-    int *ptr;
-    ptr = (int*)malloc(b);
-    if (ptr == NULL)
-    {
-        exit(98);
-    }
-    
-    return (ptr);
+int *ptr;
+ptr = (int *)malloc(b);
+if (ptr == NULL)
+{
+exit(98);
+}
+return (ptr);
 }
